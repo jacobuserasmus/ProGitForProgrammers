0a7e71106a1955cbb7e5274312f33519ada09e68
Jacobus Erasmus <jacobus@ivyweb.co.za>
2021-07-24 12:41:12 +0200

    Initial Program commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace ProGitProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("I just added this in Rider");
+        }
+    }
+}
