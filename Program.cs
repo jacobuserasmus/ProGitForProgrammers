f9070d9d60390fc31290a0d452594f51e941de5e
Jacobus Erasmus <jacobus@ivyweb.co.za>
2021-07-24 13:07:24 +0200

    Add writeline indicating we are in command line

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Rider");
+            Console.WriteLine("I just added this in the command line repo");
         }
     }
 }
