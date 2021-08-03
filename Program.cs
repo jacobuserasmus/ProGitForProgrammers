69af9b830900bac8fbcae9f92a52cd8964fefb41
Jacobus Erasmus <jacobus@ivyweb.co.za>
2021-08-03 11:02:54 +0200

    Added original Program to directory

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Rider");
+            Console.WriteLine("I just added this in the command line repo");
+        }
+    }
+}
