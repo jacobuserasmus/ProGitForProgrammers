6ce98680c5dcc04d232e873c3cb92e8d56c3f9b9
Jacobus Erasmus <jacobus@ivyweb.co.za>
2021-08-03 10:58:03 +0200

    Add calculator class

--- Program.cs
+++ Program.cs
@@ -2,13 +2,8 @@
 
 namespace ProGitForProgrammers
 {
-    class Program
+    public class Calculator
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("I just added this in Rider");
-            Console.WriteLine("I just added this in the command line repo");
-        }
+        
     }
 }
